e9593d96b1a9b2bdd1747beefd134dd386df3d9f
arnif <arnifa@gmail.com>
2015-02-23 12:35:14 +0000

    major changes

---
+++ HTML/script.js
@@ -0,0 +1,91 @@
+var HospitalContainer = React.createClass({
+	getInitialState: function() {
+		return {
+			selected: 'none',
+			hospitalData: {}
+		}
+	},
+	changeSelected: function(e) {
+		console.log(e.currentTarget.getAttribute('id'));
+		this.setState({
+			selected: e.currentTarget.getAttribute('id')
+		});
+	},
+	componentDidMount: function() {
+		console.log('AJAX');
+		var that = this;
+		$.get('http://apis.is/hospital', function(data) {
+			console.log(data);
+			console.log(that);
+			that.setState({
+				hospitalData: data.results[0]
+			});
+			console.log(that.state.hospitalData);
+		});
+	},
+	updateData: function(e) {
+		
+		var sel = e.currentTarget.getAttribute('id') //TODO send to API
+var that = this;
+		$.get('http://apis.is/hospital', function(data) {
+			console.log(data);
+			that.setState({
+				hospitalData: data.results[0],
+				selected: sel
+			});
+			console.log(that.state.hospitalData);
+		});
+	},
+	render: function() {
+		return (
+			<div>
+				<h3>{this.state.selected}</h3>
+				<HospitalButtons makeSelected={this.updateData} />
+				<HospitalView hospitalData={this.state.hospitalData} />
+			</div>
+			
+		)
+	}
+});
+
+var HospitalButtons = React.createClass({
+	handleSelected: function(e) {
+		this.props.makeSelected(e);
+	},
+	render: function() {
+		return (
+			<div>
+				<button onClick={this.handleSelected} id="last-year">Last year</button>
+				<button onClick={this.handleSelected} id="last-month">Last 30 days</button>
+				<button onClick={this.handleSelected} id="last-day">Last 7 days</button>
+			</div>
+		)
+	}
+});
+
+var HospitalView = React.createClass({
+	getDefaultProps: function() {
+		return {
+			hospitalData: {}
+		}
+	},
+	render: function() { //TODO display data
+		var that = this;
+		var objectItems = [] 
+		Object.getOwnPropertyNames(this.props.hospitalData).forEach(function(val, idx, array) {
+			//console.log(val + ' -> ' + that.props.hospitalData[val]);
+  							objectItems.push(<li>{val + ' -> ' + that.props.hospitalData[val]}</li>)
+						});
+		// console.log(objectItems);
+		return (
+			<div>
+				<ul>
+					<li>{objectItems}</li>
+				</ul>
+			</div>
+		)
+	}
+});
+
+React.render(<HospitalContainer />, document.getElementById('app'));
+
--- app.js
+++ app.js
@@ -3,26 +3,27 @@ var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
 var Hospitals = require('./hospital.model');
+var crontab = require('node-crontab');
 
 mongoose.connect('mongodb://localhost/hospital');
 
-setInterval (function() {
+var jobId = crontab.scheduleJob("55 * * * *", function(){
+    addToDatabase();
+});
+
+function addToDatabase() {
     request('http://apis.is/hospital', function (error, response, body) {
       if (!error && response.statusCode == 200) {
-     var hos = new Hospital();
+     var hos = new Hospitals();
         hos.hospitalData = JSON.parse(body);
         hos.save(function(err) {
             if (err) {
                 console.log(err);
-                //process.exit(1);
             }
-            //process.exit(0);
         });
       }
     });
-}, 3600000);
-
-
+}
 
 app.get('/hospitals', function (req, res) {
   
@@ -33,7 +34,6 @@ app.get('/hospitals', function (req, res) {
     return res.json(response);
   })
 });
-
 
 app.get('/hospitals/:from/:to', function (req, res) {
   var from = req.params.from;
