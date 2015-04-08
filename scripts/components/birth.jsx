fa33ad57c1b0b8826eb2db6dbc3883a850ebf250
Axel <selii93@gmail.com>
2015-04-08 11:22:15 +0000

    its the final countdown :snail:

--- scripts/components/birth.jsx
+++ scripts/components/birth.jsx
@@ -11,12 +11,12 @@ let Birth = React.createClass({
 	render() {
 		return (
 			<div className={this.props.className}>
-				<div className="text-bellow">
+				<div className="text-bellow" data-center-bottom="top: 0%;" data-top="top:15%;">
 					{this.props.selected} were born
+					<img className="preggo" src="../../img/preggo.svg"></img>
+					<img className="fetus" data-bottom="top: 43%; opacity: 1; left: 50%;" data-center-bottom="top: 88%; left: 60%; opacity: 0;" src="../../img/fetus.svg"></img>
+					<img className="newborn" data-center-top="top: 80%; opacity: 0; left: 61%; -webkit-transform: rotate(35deg);" data-center-bottom="top: 88%; left: 61%; opacity: 1; -webkit-transform: rotate(0deg);" src="../../img/baby.svg"></img>
 				</div>
-				<img className="preggo" data-bottom="top: 30%;" data-top="top:55%;"  src="../../img/preggo.svg"></img>
-				<img className="fetus" data-bottom="top: 58%; opacity: 1; left: 50%;" data-center-bottom="top: 88%; left: 60%; opacity: 0;" src="../../img/fetus.svg"></img>
-				<img className="newborn" data-center-top="top: 80%; opacity: 0; left: 61%; -webkit-transform: rotate(35deg);" data-center-bottom="top: 88%; left: 61%; opacity: 1; -webkit-transform: rotate(0deg);" src="../../img/baby.svg"></img>
 			</div>
 		);
 	}
