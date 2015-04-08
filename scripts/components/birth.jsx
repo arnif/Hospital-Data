5c7ee69b39353c770e87b41f2edb33b348ab8e17
Axel <selii93@gmail.com>
2015-04-08 12:25:51 +0000

    fyrir arna :hamster:

--- scripts/components/birth.jsx
+++ scripts/components/birth.jsx
@@ -17,8 +17,8 @@ let Birth = React.createClass({
 				<div className="text-bellow" data-center-bottom="top: 0%;" data-top="top:15%;">
 					{this.props.selected} were born
 					<img className="preggo" src="../../img/preggo.svg"></img>
-					<img className="fetus" data-bottom="top: 43%; opacity: 1; left: 50%;" data-center-bottom="top: 88%; left: 60%; opacity: 0;" src="../../img/fetus.svg"></img>
-					<img className="newborn" /*data-center-top="top: 80%; opacity: 0; left: 61%; -webkit-transform: rotate(35deg);" data-center-bottom="top: 88%; left: 61%; opacity: 1; -webkit-transform: rotate(0deg);"*/ src="../../img/baby.svg"></img>
+					<img className="fetus" data-bottom="top: 43vh; opacity: 1; left: 50%;" data-center-bottom="top: 44vh; left: 60%; opacity: 0;" src="../../img/fetus.svg"></img>
+					<img className="newborn" data-center-top="top: 43vh; opacity: 0; left: 61%; -webkit-transform: rotate(35deg);" data-center-bottom="top: 47vh; left: 61%; opacity: 1; -webkit-transform: rotate(0deg);" src="../../img/baby.svg"></img>
 				</div>
 			</div>
 		);
